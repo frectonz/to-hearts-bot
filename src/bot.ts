77053c8358532418e8c1bcca3df29568c9815c91
frectonz <fraol0912@gmail.com>
2024-03-22 21:54:41 +0300

    implement encoding

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,40 @@
-import { Bot } from "grammy";
+import { Bot, InlineQueryResultBuilder } from "grammy";
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN")!;
 const bot = new Bot(BOT_TOKEN);
 
+const hearts = ["❤️", "🧡", "💛", "💚", "💙", "💜", "🖤", "🤍"];
+
+function toHeart(base8: number) {
+  const el = hearts[base8];
+  return el ? el : "x";
+}
+
 bot.on("inline_query", (ctx) => {
   const query = ctx.inlineQuery.query;
-  console.log(query);
+
+  if (query.length === 0) return;
+
+  const encoder = new TextEncoder();
+  const utf8 = encoder.encode(query);
+
+  let base8: number[][] = [];
+  utf8.forEach((x) => {
+    const num = x.toString(8).split("").map((x) => parseInt(x));
+    base8.push(num);
+  });
+  base8 = base8.reverse();
+
+  const hearts = base8.map((group) => group.map((x) => toHeart(x)));
+  const heartsMsg = hearts.map((group) => group.join("")).join(" ");
+
+  const encodeResult = InlineQueryResultBuilder.article(
+    query,
+    "encoded message",
+    { description: heartsMsg },
+  ).text(heartsMsg);
+
+  ctx.answerInlineQuery([encodeResult]);
 });
 
 export default bot;
